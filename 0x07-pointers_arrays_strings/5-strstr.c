641a6c553d2ade61b71b25f1ce95a4038b2cfd00
Lorso-geek <ulorsophia@gmail.com>
2023-04-03 13:01:49 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -4,9 +4,9 @@
  * @haystack: input
  * @needle: input
  * Return: Always 0 (Success)
- */	
+ */
 char *_strstr(char *haystack, char *needle)
-{	
+{
 	for (; *haystack != '\0'; haystack++)
 	{
 		char *l = haystack;
